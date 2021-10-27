dbcc9cd3dd38d4d7d55312aa41dee9a89d2c5c7b
Dominick Tecson <dominicktecson@gmail.com>
2021-10-27 15:52:46 -0700

    Initial commit

---
+++ quotes.js
@@ -0,0 +1,151 @@
+const localQuotes = [
+    {
+      text: "What do you cook your oil in?",
+      author: "Margaret Tecson"
+    },
+    {
+        text: "The apple doesn’t fall from the tree",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "No harm no done",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "A can of pantry mover (a can of paint remover)",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "What year is the strongest is great strawberries",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Let's get down to the meat and grits/bones",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "You just took it out and you’re literally putting half a bag a box ",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Let’s staple it in (Pencil in the date) ",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "It’s definitely imbreaded in them ",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "If he wakes up dead, I’ll kill you",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "This guy def not would  ",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "I’ll see it when I believe it",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "You get what you see (what you see is what you get)",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "The airport picked us up",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Now it’s going to be lumpsided",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Do we have any hand wash?",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "I was supposed to go but I slid under the rocks",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "He had his nose in his fingers",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "I think I should get the flu shot, there’s different strands (strains) out there",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Babe, you’re in the shadow",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "You never get what you like...you get what you need ",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "I don’t even put tea in my sugar",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Ya, he used to when puppy was a Billy",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "What’s 100 feet in square feet",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "It’s crazy that the snow got mountains",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "This is Karen’s moms room with full tower shub ",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "I shoulda got a shly fwatter",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Omg have you touched your hands?!",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "This place looks like a pigstine ",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "This is what happens in the flash of an eye",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Should we just go to ranch chino? ...what, I’m just missing the 9-9",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Hey GooGoo",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "How much thing does this weigh?",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Oh, I should keep the luggage open for her clothes. Why? It’s just one clothe.",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Can you turn down the lightage?",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "Earlier today I was BLOISTERING",
+        author: "Margaret Tecson"
+      },
+      {
+        text: "That’s the thing about these women, there’s such a broad spectum. ",
+        author: "Margaret Tecson"
+      },
+    
+]
