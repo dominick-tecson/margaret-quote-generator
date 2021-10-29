95f676412e8e3fd40bf6b6fdf95a8d2959b02b03
Dominick Tecson <dominicktecson@gmail.com>
2021-10-29 09:35:03 -0700

    Added more JS functionality

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
     // Pick a random quote from apiQuotes array
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
-    console.log(quote);
+    // Check if author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check quote length to determine styling
+    if (quote.text.lenght > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
+
+// On Load
+newQuote();
 
 // Get Quotes From API
 // async function getQuotes() {
@@ -19,6 +40,15 @@ function newQuote() {
 //     }
 // }
 
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // // On Load
 // getQuotes();
-newQuote();
